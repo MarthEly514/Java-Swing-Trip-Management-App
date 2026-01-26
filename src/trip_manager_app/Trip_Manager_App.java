ec340373d1f02cf6da7260c66c222a480aaaee05
Ely <martharun514@gmail.com>
2026-01-26 20:35:05 +0100

    initialisation

---
+++ src/trip_manager_app/Trip_Manager_App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trip_manager_app;
+
+/**
+ *
+ * @author ely
+ */
+public class Trip_Manager_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
