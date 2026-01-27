aa7051287950f043dec40a868ec8abe0c7f0cf95
Ely <martharun514@gmail.com>
2026-01-27 10:25:51 +0100

    Project structure initialization

--- src/trip_manager_app/Trip_Manager_App_Main.java
+++ src/trip_manager_app/Trip_Manager_App_Main.java
@@ -8,14 +8,14 @@ package trip_manager_app;
  *
  * @author ely
  */
-public class Trip_Manager_App {
+public class Trip_Manager_App_Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world nigger");
+        
     }
     
 }
